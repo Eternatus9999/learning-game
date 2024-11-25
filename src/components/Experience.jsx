671c57c63ed821b432723d087eb31a458d452fbd
Eternatus9999 <eternatus9999@gmail.com>
2024-11-25 20:40:30 +0530

    Add Laptop 3D Model to the project

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,25 @@
-import { OrbitControls } from "@react-three/drei";
+import { Cylinder, OrbitControls } from "@react-three/drei";
+import { CylinderCollider, RigidBody } from "@react-three/rapier";
+import { Lap } from "./Lap";
 
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      {/* LIGHTS */}
+      <ambientLight intensity={1} />
+      <directionalLight position={[5, 1, 5]} intensity={0.8} castShadow />
+      
+      {/* BACKGROUND */}
+      <Lap scale={[60, 60, 60]}/>
+
+      {/* STAGE */}
+      <RigidBody type="fixed" position={-0.5} colliders={false}>
+        <CylinderCollider args={[1/2, 5]} />
+        <Cylinder scale={[5, 1, 5]} receiveShadow>
+          <meshStandardMaterial color="white" />
+        </Cylinder>
+        </RigidBody>
     </>
   );
 };
